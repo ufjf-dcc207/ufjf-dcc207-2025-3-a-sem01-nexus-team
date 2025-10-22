69633e38b98f97726ee5f387bef443504ea910d3
Gianlucca Paiva <gianluccapaiva@gmail.com>
2025-10-22 09:46:33 -0300

    idealização inicial do box de personagem na home page by Gianlucca

---
+++ src/Personagem.tsx
@@ -0,0 +1,16 @@
+type Classificacao = 1|2|3|4|5;
+
+import './Personagem.css';
+export default function Personagem({nome, imagem, nivelPerigo, status, idade, dataNascimento}: 
+    {nome: string, imagem: string, nivelPerigo: Classificacao, status: boolean, idade: number, dataNascimento: string}) {
+    return(
+        <div className="personagem">
+            <div className="nome"><h2>{nome}</h2></div>
+            <div className="imagem"><img src={imagem} alt={nome} /></div>
+            <div className="nivel-perigo"><p>Nível de Perigo: {nivelPerigo}</p></div>
+            <div className="status"><p>Status: {status? "Capturado" : "Foragido"}</p></div>
+            <div className="idade"><p>Idade: {idade}</p></div>
+            <div className="data-nascimento"><p>Data de Nascimento: {dataNascimento}</p></div>
+        </div>
+    );
+}
