32a943888dce163ae7d617438082712f94bf79e5
Gianlucca Paiva <gianluccapaiva@gmail.com>
2025-10-29 16:45:04 -0300

    orientação da recompensa com as stars

--- src/Personagem.tsx
+++ src/Personagem.tsx
@@ -58,11 +58,11 @@ export default function Personagem({nome, subnome, imagem, nivelPerigo, status, 
             <div className='subnome'><h3>{subnome}</h3></div>
             <div className="imagem"><img src={imagem} alt={nome} /></div>
             <div className="nivel-perigo"><p>Nível de Perigo: </p></div>
-            <div className='estrela'><span>{'⭐'.repeat(nivelPerigo) + '☆'.repeat(5 - nivelPerigo)}</span></div>
+            <div className='estrela'><p>{'⭐'.repeat(nivelPerigo) + '☆'.repeat(5 - nivelPerigo)}</p></div>
             <div className="status"><p>Status: {situacao}</p></div>
             <div className="idade"><p>Idade: <span className={desconhecidoIdade}>{idade}</span></p></div>
             <div className="data-nascimento"><p>Data de Nascimento: <span className={desconhecidoData}>{dataFormatada}</span></p></div>
-            <div className="recompensa"><p>Recompensa: R${recompensa.toFixed(2).replace('.', ',')}</p></div>
+            <div className="recompensa"><p>Recompensa: {status === "Foragido" || status === "Desconhecido" ? `R$${recompensa.toFixed(2).replace('.', ',')}` : "Indisponível"}</p></div>
         </div>
     );
 }
