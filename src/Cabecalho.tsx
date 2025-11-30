284da07b1c240f9a7e3ccebe4c252530a881dc69
GabrielLineker <gabriel.lineker@estudante.ufjf.br>
2025-11-30 00:24:45 -0300

    Mais ajustes nas estilizações do cabeçalho

--- src/Cabecalho.tsx
+++ src/Cabecalho.tsx
@@ -3,11 +3,13 @@ export function Cabecalho(){
     return(
         <>
             <div className="cabecalho-departamento">
-                <img
-                src="Logos/DPNClogo.png"
-                alt="Logo do Departamento"
-                className="logo-departamento"
-                />
+                <div className="logo-container">
+                    <img
+                    src="Logos/DPNClogo.png"
+                    alt="Logo do Departamento"
+                    className="logo-departamento"
+                    />
+                </div>
                 <div className="conteiner-navegacao">
                     <h2 className="nome-departamento">
                     Departamento  de Policia Nexus City
