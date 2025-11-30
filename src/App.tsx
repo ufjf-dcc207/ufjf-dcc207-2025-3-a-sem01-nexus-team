9490bc3f684d98756571fa0f8d316f3f16181503
GabrielLineker <gabriel.lineker@estudante.ufjf.br>
2025-11-30 16:07:29 -0300

    Criação da barra de filtragem funcional

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,62 @@ import Personagem from "./Personagem";
 import InterfaceExibicao from "./InterfaceExibicao";
 import { ListaProcurados } from "./ListaProcurados";
 import { filtrarPersonagem } from "./utilitarios/utils";
+import { useState } from "react";
 
 function App() {
-  const nome = "";
-  const status = "";
-  const estrela = 0;
 
+  const [nome, setNome] =  useState("");
+  const [status, setStatus] = useState("");
+  const [estrela, setEstrela] = useState(0);
   const ListaProcuradosFiltrado = filtrarPersonagem(ListaProcurados, nome, status, estrela);
 
   return (
     <div className="App">
-      <h1 className="titulo-principal">PROCURADOS</h1>
+      
+      <div className="interface-procurados">
+        <h1 className="titulo-principal">PROCURADOS</h1>
+
+        <div className="container-busca">
+
+          <div className="interface-busca">
+            <input
+              type="text"
+              className="campo-texto"
+              placeholder="Buscar por nome..."
+              value={nome}
+              onChange={(texto) => setNome(texto.target.value)}
+            />
+
+            <select 
+                className="campo-select divisa-esquerda" 
+                value={estrela} 
+                onChange={(estrela) => setEstrela(Number(estrela.target.value))}
+                title="Filtrar por Nível"
+            >
+                <option value={0}>⭐</option>
+                <option value={1}>⭐</option>
+                <option value={2}>⭐⭐</option>
+                <option value={3}>⭐⭐⭐</option>
+                <option value={4}>⭐⭐⭐⭐</option>
+                <option value={5}>⭐⭐⭐⭐⭐</option>
+            </select>
+
+            <select 
+                className="campo-select divisa-esquerda" 
+                value={status} 
+                onChange={(status) => setStatus(status.target.value)}
+                title="Filtrar por Status"
+            >
+                <option value="">🚨</option>
+                <option value="Foragido">Foragido</option>
+                <option value="Capturado">Capturado</option>
+                <option value="Morto">Morto</option>
+                <option value="Desconhecido">Desconhecido</option>
+            </select>
+          </div>
+        </div>
+
+      </div>
       <InterfaceExibicao>
         {ListaProcuradosFiltrado.map((personagem) => (
           <div className="card" key ={personagem.id}>
