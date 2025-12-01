c63183cf407096b09cea5c940449e1d5208a0602
GabrielLineker <gabriel.lineker@estudante.ufjf.br>
2025-11-30 21:33:40 -0300

    Criação do componente ficha

--- src/Ficha.tsx
+++ src/Ficha.tsx
@@ -1,2 +1,43 @@
 import "./estilos/Ficha.css";
+import type{ Status } from "./Personagem";
 
+import {situacaoStatus, statusValido, idadeValida, nivelPerigoValido, trataRecompensa, 
+    formataIdade, checaDataNascimento, trataData} from './utilitarios/utils';
+
+export default function Ficha({nome, subnome, imagem, nivelPerigo, status, idade, dataNascimento, recompensa, descricao, crimes, ultimaLocalizacao}: 
+    {nome: string, subnome: string, imagem: string, nivelPerigo: number, status: Status, idade: number | string, dataNascimento: string, recompensa: number, descricao: string, crimes: string[], ultimaLocalizacao: string}) {
+    let desconhecidoData: string = "";
+    let dataFormatada: string;
+    let desconhecidoIdade: string = "";
+    let situacao;
+    let recompensaValida;
+
+    status = statusValido(status);
+    situacao = situacaoStatus(status);
+    nivelPerigo = nivelPerigoValido(nivelPerigo);
+    desconhecidoData = checaDataNascimento(dataNascimento);
+    dataFormatada = trataData(dataNascimento);   
+    idade = idadeValida(idade);
+    desconhecidoIdade = formataIdade(idade);
+    recompensaValida = trataRecompensa(recompensa, status);
+
+    return(
+        <div className="ficha">
+            <div className="nome-ficha"><h2>{nome}</h2></div>
+            <div className='subnome-ficha'><h3>{subnome}</h3></div>
+            <div className="imagem-ficha"><img src={imagem} alt={nome} /></div>
+            <div className="nivel-perigo-ficha"><p>Nível de Perigo: </p></div>
+            <div className='estrela-ficha'><p>{'⭐'.repeat(nivelPerigo) + '☆'.repeat(5 - nivelPerigo)}</p></div>
+            <div className="status-ficha"><p>Status: <span className={situacao}>{status}</span></p></div>
+            <div className="idade-ficha"><p>Idade: <span className={desconhecidoIdade}>{idade}</span></p></div>
+            <div className="data-nascimento-ficha"><p>Nascimento: <span className={desconhecidoData}>{dataFormatada}</span></p></div>
+            <div className="recompensa-ficha"><p>Recompensa: {recompensaValida}</p></div>
+            <div className="descricao-ficha"><p>{descricao}</p></div>
+            <div className="crimes-ficha"><p><strong>Crimes:</strong> {crimes.join(', ')}</p></div>
+            <div className="ultima-localizacao-ficha"><p>Última Localização: {ultimaLocalizacao}</p></div>
+            <button className="botao-voltar">Voltar</button>
+            <button className="botao-pegar-recompensa">Pegar Recompensa</button>
+        </div>
+    );
+}
+    
